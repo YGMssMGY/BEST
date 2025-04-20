964b0876f97f2f1eb5ab0c8ee927d18ebb4e7021
richie-rich90454 <zhiyu.jiang90454-bisz@basischina.com>
2025-04-20 10:34:19 +0800

    made it ipv6 only

--- server.js
+++ server.js
@@ -57,7 +57,7 @@ fastify.get("/pdf-to-img", async (request, reply)=>{
 });
 async function start(){
     await ensureImagesDir();
-    await fastify.listen({ port, host: "0.0.0.0"});
+    await fastify.listen({ port, host: "::"});
     console.log(`Server running on http://localhost:${port}`);
 }
 start().catch(err=>{
